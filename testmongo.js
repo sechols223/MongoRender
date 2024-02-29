12af7b060c839e8375589677e1e879ffd0b0b8d6
Samuel Echols <samuel.echols@selu.edu>
2024-02-29 14:11:36 -0600

    Made changes

--- testmongo.js
+++ testmongo.js
@@ -1,14 +1,15 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+	"mongodb+srv://dbuser:J7IklKss03UQBrxj@learningcluster.zcjavig.mongodb.net/?retryWrites=true&w=majority&appName=LearningCluster";
 
 // --- This is the standard stuff to get it to work on the browser
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log("Server started at http://localhost:" + port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,41 +17,38 @@ app.use(express.urlencoded({ extended: true }));
 // routes will go here
 
 // Default route:
-app.get('/', function(req, res) {
-  const myquery = req.query;
-  var outstring = 'Starting... ';
-  res.send(outstring);
+app.get("/", function (req, res) {
+	const myquery = req.query;
+	var outstring = "Starting... ";
+	res.send(outstring);
 });
 
-app.get('/say/:name', function(req, res) {
-  res.send('Hello ' + req.params.name + '!');
+app.get("/say/:name", function (req, res) {
+	res.send("Hello " + req.params.name + "!");
 });
-
 
 // Route to access database:
-app.get('/api/mongo/:item', function(req, res) {
-const client = new MongoClient(uri);
-const searchKey = "{ partID: '" + req.params.item + "' }";
-console.log("Looking for: " + searchKey);
+app.get("/api/mongo/:id", function (req, res) {
+	const client = new MongoClient(uri);
+	const searchKey = "{ id: '" + req.params.id + "' }";
+	console.log("Looking for: " + searchKey);
 
-async function run() {
-  try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+	async function run() {
+		try {
+			const database = client.db("sample_airbnb");
+			const parts = database.collection("listingsAndReviews");
 
-    // Hardwired Query for a part that has partID '12345'
-    // const query = { partID: '12345' };
-    // But we will use the parameter provided with the route
-    const query = { partID: req.params.item };
+			const id = req.params.id;
+			console.log(id);
+			const query = { id: req.params.id };
 
-    const part = await parts.findOne(query);
-    console.log(part);
-    res.send('Found this: ' + JSON.stringify(part));  //Use stringify to print a json
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+			const part = await parts.findOne(query);
+			console.log(part);
+			res.send("Found this: " + JSON.stringify(part)); //Use stringify to print a json
+		} finally {
+			// Ensures that the client will close when you finish/error
+			await client.close();
+		}
+	}
+	run().catch(console.dir);
 });
